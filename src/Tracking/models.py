00b95398044c16fc0872802536273e0cae9af5b9
Amir Mallaei <amirmallaei@gmail.com>
2025-01-18 11:05:55 +0330

    Model added

--- src/Tracking/models.py
+++ src/Tracking/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserActivity(models.Model):
+    ACTION_CHOICES = [
+        ('login', 'Login'),
+        ('logout', 'Logout'),
+        ('create_post', 'Create Post'),
+        ('update_profile', 'Update Profile'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='USER')
+    action = models.CharField(max_length=50, choices=ACTION_CHOICES, verbose_name='ACTION')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='DATE and TIME')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.action} at {self.timestamp}"
